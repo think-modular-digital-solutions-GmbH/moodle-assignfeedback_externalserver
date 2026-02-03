a03ebee0d76bfa7a16a758298dbd757a0f45d9c4
Stefan Weber <stefan.weber@think-modular.com>
2026-02-03 09:13:41 +0100

    Add German langstrings

---
+++ lang/de/assignfeedback_externalserver.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for the external server feedback plugin.
+ *
+ * @package    assignfeedback_externalserver
+ * @author     Stefan Weber (stefan.weber@think-modular.com)
+ * @copyright  2025 think-modular
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['batchlabel'] = 'Bewertungen vom externen Server holen';
+$string['enabled'] = 'Aktiv';
+$string['enabled_help'] = 'Aktiviert das holen von Bewertungen und Feedbacks für mehrere Teilnehmer/innen auf der Abgabeübersichtsseite, indem dort eine Aktion hinzugefügt wird.';
+$string['getgradesfromexternalserver'] = 'Bewertungen vom externen Server holen';
+$string['getgradesfromexternalserver_confirm'] = 'Möchten Sie wirklich die Bewertungen und Feedbacks für die ausgewählten Abgaben vom externen Server holen? Das überschreibt existierende Bewertungen und Feedbacks und kann nicht rückgängig gemacht werden.';
+$string['pluginname'] = 'Bewertungen für Abgaben vom externen Server holen';
+$string['privacy:metadata'] = 'Das Plugin für externe Server-Feedbacks speichert keine personenbezogenen Daten.';
